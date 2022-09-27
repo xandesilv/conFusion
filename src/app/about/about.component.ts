f8130f991743410988dcb25a848be9494e00aa21
Alexandre Silva <alexandre.b.silva@capgemini.com>
2022-09-27 11:51:59 -0300

    Animations parte 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
-
+import { flyInOut, expand } from '../animations/app.animation';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 export class AboutComponent implements OnInit {
 leaders: Leader[];
 
-  constructor(private leaderservice: LeaderService) { }
+  constructor(private leaderservice: LeaderService,
+    @Inject('BaseURL') public BaseURL) { }
 
   ngOnInit(): void {
     this.leaderservice.getLeaders()
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,25 +8,21 @@ import { switchMap } from 'rxjs';
 //ATÉ AQUI ---------------
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations'
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.3s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
 
 })
